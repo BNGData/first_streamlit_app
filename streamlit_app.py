8d9cd4e0832bb00cdc99e296acf205b789ce3e08
Rob C <120201365+BNGData@users.noreply.github.com>
2022-12-09 08:48:08 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.title('My Parents New Healty Diner')
 streamlit.subheader ('Breakfast Menu')
 
 streamlit.text ('Omega 3 & Blueberry Oat Meal') 
+streamlit.text ('Kale, Spinach & Rocket Smoothie') 
+streamlit.text ('Hard-Boiled Free-Range Egg') 
